ad0f9fb847b818e997cba8ffbcb22eea687307e4
Geethi Tarra <gtarra@calpoly.edu>
2024-10-22 23:30:55 -0700

    lab

--- hw1.py
+++ hw1.py
@@ -1,28 +1,67 @@
-import data
-
+from data import Book, Rectangle, Price, Circle
 # Write your functions for each part in the space below.
 
 # Part 1
+def vowel_count(string):
+    count = 0
+    for char in string:
+        if char == "a" or char == "e" or char == "i" or char == "o" or char == "u" or char == "A" or char == "E" or char == "I" or char == "O" or char == "U":
+            count += 1
+    return count
 
 
 # Part 2
+def short_lists(lst: list[list[int]]):
+    result = []
+    for element in lst:
+        if len(element) == 2:
+            result.append(element)
+    return result
 
 
 # Part 3
-
+def ascending_pairs(lst: list[list[int]]):
+    result = []
+    for element in lst:
+        if len(element) == 2:
+            if element[0] < element [1]:
+                result.append(element)
+            elif element[0] > element [1]:
+                result.append([element[1], element[0]])
+        else:
+            result.append(element)
+    return result
 
 # Part 4
+def add_prices(price1: Price, price2: Price):
+    total = price1.cents + price2.cents
+    dollar = 0
+    cents = 0
+    if total > 100:
+        dollar = total//100
+        cents = total % 100
 
+    return price1.dollars + price2.dollars + dollar + cents
 
 # Part 5
+def rectangle_area(rectangle: Rectangle):
+    xl, yl = rectangle.top_left.x, rectangle.top_left.y
+    xb, yb = rectangle.bottom_right.x, rectangle.bottom_right.y
+
+    return (xb-xl)*(yb-yl)
 
 
-# Part 6
-
+# Part 6 edit?
+def books_by_author(author: str, books: list[Book]):
+   result = []
+   for book in books:
+       for book_author in book.authors:
+           if book_author == author:
+               result.append(book)
+   return result
 
 # Part 7
 
 
 # Part 8
-
 
--- hw1_test.py
+++ hw1_test.py
@@ -1,6 +1,5 @@
-import data
-import hw1
 import unittest
+from hw1 import vowel_count, short_lists, ascending_pairs
 
 
 # Write your test cases for each part below.
@@ -8,12 +7,43 @@ import unittest
 class TestCases(unittest.TestCase):
     # Part 1
 
+    def test_vowel_count(self):
+        test = "Hello"
+        result = vowel_count(test)
+        expected = 2
+        self.assertEqual(expected, result)
+
+
+    def test_vowel_count2(self):
+        test = "pl"
+        result = vowel_count(test)
+        expected = 0
+        self.assertEqual(expected, result)
 
     # Part 2
-
+    def test_short_lists(self):
+        test = [[4], [5], [6, 7], [8, 9, 10]]
+        result = short_lists(test)
+        expected = [[6, 7]]
+        self.assertEqual(expected, result)
+    def test_short_lists2(self):
+        test = [[4], [5], [6, 7, 8], [8, 9, 10]]
+        result = short_lists(test)
+        expected = []
+        self.assertEqual(expected, result)
 
     # Part 3
+    def test_ascending_pairs(self):
+        test = [[4], [5], [6, 7], [8, 9, 10]]
+        result = ascending_pairs(test)
+        expected = [[4], [5], [6,7], [8, 9, 10]]
+        self.assertEqual(expected, result)
 
+    def test_ascending_pairs2(self):
+        test = [[4], [5], [10, 7], [8, 9, 10]]
+        result = ascending_pairs(test)
+        expected = [[4], [5], [7, 10], [8, 9, 10]]
+        self.assertEqual(expected, result)
 
     # Part 4
 
@@ -33,5 +63,3 @@ class TestCases(unittest.TestCase):
 
 
 
-if __name__ == '__main__':
-    unittest.main()
